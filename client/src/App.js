b23e3cdf00cc48ec3bd4b7ec8e079f61e5ec75cd
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-02-03 12:47:34 -0500

    add bootstrap elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
+import {Button, Alert, Breadcrumb, Card} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <Button> Test button</Button>
+      <Breadcrumb>
+        <Breadcrumb.Item>Login</Breadcrumb.Item>
+        <Breadcrumb.Item>Register</Breadcrumb.Item>
+        <Breadcrumb.Item>Logout</Breadcrumb.Item>
+      </Breadcrumb>
+      <Card>
+        <Card.Img/>
+        <Card.Body>
+          <Card.Title>
+            Image Results
+          </Card.Title>
+          <Alert variant="danger">Danger!</Alert>
+          <Card.Text>
+            Wheat contains gluten!
+          </Card.Text>
+          <Button variant="secondary">Read More</Button>
+        </Card.Body>
+      </Card>
+      <Alert variant="primary">This is a button</Alert>
+      <Button>Next</Button>
     </div>
   );
 }

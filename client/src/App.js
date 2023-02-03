c39bc815aab5e6972751f6cf0e9e0fa5886f8e73
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-02-03 12:39:28 -0500

    Add sass and bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button> Test button</Button>
     </div>
   );
 }

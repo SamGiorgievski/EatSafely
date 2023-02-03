d5b6e9257898bd7625f758d32c0dfe4fe9b44a8b
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-02-03 10:14:29 -0500

    setup back-end

---
+++ server/routes/catsRoutes.js
@@ -0,0 +1,12 @@
+// -- routes/catRoutes.js
+const router = require('express').Router();
+const users = require('../db/queries/users');
+
+router.get('/', (req, res) => {
+  users.getAllUsers().then(data => {
+    console.log(data);
+    res.json({users: data});
+  })
+});
+  
+module.exports = router;

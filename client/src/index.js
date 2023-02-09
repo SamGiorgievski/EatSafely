75435d7d8c542ad31a7b419cb069a3aa04e95139
Dihni Hassan <dihnihassan@gmail.com>
2023-02-09 18:26:01 -0500

    TYpo

--- client/src/index.js
+++ client/src/index.js
@@ -27,7 +27,7 @@ export default function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/travelcard" element={<Translate />} />
             <Route path="/profile" element={<Profile getIntolerances={setIntolerances}/>} />
-            <Route path="*" element={< NotFound />}/>
+            <Route path="*" element={<NotFound />}/>
           </Route>
 
 

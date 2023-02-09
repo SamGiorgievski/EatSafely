c33ce4f3a5d92fe5fbe4bbad6402641e0f10d85e
Samuel Giorgievski <samgiorgievski@gmail.com>
2023-02-09 16:13:59 -0500

    implement highlighter feature

--- client/src/pages/scan_result.jsx
+++ client/src/pages/scan_result.jsx
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import "./scan_result.scss";
 
-export default function ScanResult({ocrState, confidence, searchText, intolerances}) {
+export default function ScanResult({ocrState, confidence, intolerances}) {
 
-  useEffect(() => {
-    console.log(`useEffect intolerances: ${intolerances}`);
-    console.log(`useEffect text: ${ocrState.text}`);
-  }, [ocrState, intolerances]);
+  // useEffect(() => {
+  //   findMatches
+  // }, [findMatches]);
 
   function findMatches(intolerances, str) {
     let matches = [];
@@ -20,28 +20,66 @@ export default function ScanResult({ocrState, confidence, searchText, intoleranc
       }
     }
 
-    // console.log(`find matches: newStr ${newStr}`);
-
     let strArray = newStr.split(" ");
-
+    // console.log(`find matches: newStr ${newStr}`);
     // console.log(`find matches: strArray ${strArray}`);
     // console.log(`find matches: strArray ${typeof strArray}`);
-
     // console.log(`find matches: intolerances - ${intoleranceArray}`)
     // console.log(`find matches: intolerances - ${typeof intoleranceArray}`)
     
     for (let i = 0; i < intoleranceArray.length; i++) {
       for (let j = 0; j < strArray.length; j++) {
-        // console.log(strArray[j])
         if (intoleranceArray[i] === strArray[j]) {
           matches.push(intoleranceArray[i]);
           break;
         }
       }
     }
-    
-    // console.log(matches);
+
     return matches;
+  }
+
+  function highlightText (intolerances, str) {
+    let matches = [];
+    let newStr = "";
+    let validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ";
+    let intoleranceArray = intolerances.split(", ");
+    
+    for (let i = 0; i < str.length; i++) {
+      let char = str[i];
+      if (validChars.indexOf(char) !== -1) {
+        newStr += char;
+      }
+    }
+
+    let strArray = newStr.split(" ");
+    
+    for (let i = 0; i < intoleranceArray.length; i++) {
+      for (let j = 0; j < strArray.length; j++) {
+        if (intoleranceArray[i] === strArray[j]) {
+          matches.push(intoleranceArray[i]);
+          break;
+        }
+      }
+    }
+
+    let highlighterReturn = [];
+
+    for (let i = 0; i < matches.length; i++) {
+      for (let j = 0; j < strArray.length; j++) {
+        if (matches[i] === strArray[j]) {
+          highlighterReturn.push(<span className="highlight"> {strArray[j]} </span>);
+        } else {
+          highlighterReturn.push(<span> {strArray[j]} </span>)
+        }
+      }
+    }
+
+    return (
+      <div>
+        {highlighterReturn}
+      </div>);
+
   }
 
 
@@ -49,9 +87,12 @@ export default function ScanResult({ocrState, confidence, searchText, intoleranc
     <main className="results">
       <div className="text-box">
         <p className="results" id="inner">
-          {ocrState.text && <span> Results: {ocrState.text}</span>}
+          {/* {ocrState.text && <span> Results: {ocrState.text}</span>} */}
           {confidence > 55 && <span>High confidence : {confidence}%</span>}
-          <div className="Matches">
+          <div className="text_with_highlight">
+            {highlightText(intolerances, ocrState.text)}
+          </div>
+          <div className="matches">
             {intolerances && <span> Matches: {findMatches(intolerances, ocrState.text)}</span>}
           </div>
         </p>
